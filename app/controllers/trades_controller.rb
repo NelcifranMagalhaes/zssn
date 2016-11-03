c9373e7b13ddf944b1f32315fed2dd169695e0ae
Nelcifran Magalhães <nelcifranpires@gmail.com>
2016-11-02 22:41:59 -0200

    mudancas no controller

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -6,11 +6,42 @@ class TradesController < ApplicationController
 
 
 	def new
+		@trade = Trade.new
 
   	end
 
   	def create
 
   	end
+
+
+def create
+    @trade = Trade.new(trade_params)
+
+    respond_to do |format|
+      if @trade.save
+        format.html { redirect_to @trade, notice: 'Trade was successfully created.' }
+        format.json { render :show, status: :created, location: @trade }
+      else
+        format.html { render :new }
+        format.json { render json: @trade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
+  	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_trade
+      @trade = Trade.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def trade_params
+
+      params.require(:trade).permit(:id)
+
+    end
 
 end
